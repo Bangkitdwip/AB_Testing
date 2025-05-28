425cb5af69016923becea4dc29cdddf37e42d487
Bangkitdwip <bangkitdwip13@gmail.com>
2025-05-28 19:34:59 +0900

    home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Portofolio")
 
 st.sidebar.title('Homepage')
-page = st.sidebar.radio('Pilih halaman:', ['Home', 'Projek','Kontak'])
+page = st.sidebar.radio('Pilih halaman:', ['Home', 'Projek','Tentang Saya'])
 
 if page == 'Home':
     st.markdown("<h1 style='text-align: center;'>Welcome To My Portofolio</h1>", unsafe_allow_html=True)
